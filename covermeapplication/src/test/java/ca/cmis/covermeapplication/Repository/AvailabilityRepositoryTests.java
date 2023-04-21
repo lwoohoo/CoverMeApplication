44deffc7eedd38ba34d8a61d605d3d5901e75b38
Charlie Morse <Charles.morse@mail.mcgill.ca>
2023-04-20 20:21:57 -0400

    availability repo tests complete

--- covermeapplication/src/test/java/ca/cmis/covermeapplication/Repository/AvailabilityRepositoryTests.java
+++ covermeapplication/src/test/java/ca/cmis/covermeapplication/Repository/AvailabilityRepositoryTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -70,6 +71,76 @@ public class AvailabilityRepositoryTests {
 
         assertNotNull(availability);
         assertEquals(AvailabilityStatus.AVAILABLE, availability.getAvailabilityStatus());
+    }
+
+    @Test
+    public void testFindAvailabilityByDate() {
+        //create account
+        String name = "Peter Quinley";
+        String email = "Peter@yandex.ru";
+        String phoneNumber = "902-310-3030";
+        String username = "peteyboy";
+        String password = "password123";
+        boolean isAdmin = false;
+
+        Account account = new Account(name, email, phoneNumber, username, password, isAdmin);
+
+        account = accountRepository.save(account);
+
+        //create calendar
+        Calendar calendar = new Calendar();
+
+        calendar = calendarRepository.save(calendar);
+
+        //create availability event
+        LocalDate date = LocalDate.of(2023, 7, 20);
+        LocalTime startTime = LocalTime.of(13, 0, 0);
+        LocalTime endTime = LocalTime.of(17, 0, 0);
+
+        Availability availability = new Availability(date, startTime, endTime, account, calendar, AvailabilityStatus.AVAILABLE);
+    
+        availability = availabilityRepository.save(availability);
+        
+        List<Availability> availabilities = availabilityRepository.findAvailabilityByDate(date);
+
+        assertEquals(availabilities.size(), 1);
+        assertNotNull(availabilities.get(0));
+        assertEquals(availabilities.get(0).getDate(), date);
+    }
+
+    @Test
+    public void testFindAvailabilityByAvailabilityStatus() {
+        //create account
+        String name = "Peter Quinley";
+        String email = "Peter@yandex.ru";
+        String phoneNumber = "902-310-3030";
+        String username = "peteyboy";
+        String password = "password123";
+        boolean isAdmin = false;
+
+        Account account = new Account(name, email, phoneNumber, username, password, isAdmin);
+
+        account = accountRepository.save(account);
+
+        //create calendar
+        Calendar calendar = new Calendar();
+
+        calendar = calendarRepository.save(calendar);
+
+        //create availability event
+        LocalDate date = LocalDate.of(2023, 7, 20);
+        LocalTime startTime = LocalTime.of(13, 0, 0);
+        LocalTime endTime = LocalTime.of(17, 0, 0);
+
+        Availability availability = new Availability(date, startTime, endTime, account, calendar, AvailabilityStatus.AVAILABLE);
+    
+        availability = availabilityRepository.save(availability);
+
+        List<Availability> availabilities = availabilityRepository.findAvailabilityByDate(date);
+
+        assertEquals(availabilities.size(), 1);
+        assertNotNull(availabilities.get(0));
+        assertEquals(availabilities.get(0).getAvailabilityStatus(), Availability.AvailabilityStatus.AVAILABLE);
     }
 
 
